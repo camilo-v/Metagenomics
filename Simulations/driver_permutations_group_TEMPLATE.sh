f3bed8fe7d9ce43bb4eddfa7531d61473e6e01af
Camilo <cvaldes3@miami.edu>
2014-11-26 17:47:23 -0500

    Updated dependency and usage instructions.

--- Simulations/driver_permutations_group_TEMPLATE.sh
+++ Simulations/driver_permutations_group_TEMPLATE.sh
@@ -27,13 +27,7 @@
 #
 #   DEPENDENCIES:
 #
-#       • None. Uses standard python foundation classes.
-#
-#   The above libraries & modules are required. You can check the modules currently installed in your
-#   system by running: python -c "help('modules')"
-#
-#   USAGE:
-#   Run the program with the "--help" flag to see usage instructions.
+#       • None. Standard bash shell script.
 #
 #
 #
