849337f830ffca070f23762e81d7d04c98692fc6
Camilo <cvaldes3@miami.edu>
2014-12-02 16:04:55 -0500

    Updated header and comments.

--- HMP_Analysis/permutations.sh
+++ HMP_Analysis/permutations.sh
@@ -3,23 +3,40 @@
 
 # ---------------------------------------------------------------------------------------------------------------------
 #
-#                                   Center for Computational Science
-#										http://www.ccs.miami.edu/
-#                             			  University of Miami
+#                                   		Center for Computational Science
+#												http://www.ccs.miami.edu/
+#                             			  			University of Miami
 #
 #   This software is a "University of Miami Work" under the terms of the United States Copyright Act.
 #   Please cite the author(s) in any work or product based on this material.
 #
 #   OBJECTIVE:
-#	The purpose of this program is to drive the permutations and alignment of the HMP data.
+#	The purpose of this script is to create a single permutation of the mutated NGS reads (the sample).  This script is
+#	called N-number of times by "driver_permutations_group_TEMPLATE.sh", where "N" is the number of permutations desired.
+#	This script drives the bulk of the work: it aligns the NGS reads at different mutation rates: from '0' (no mutations)
+#	all the way to '.30' in intervals of '0.02'.
+#	The main task of this script is to mutate the reads at a given mutation rate ($PROB) by calling the perl script
+#	"sequencePermuter.pl"; it then aligns the output (the newly mutated NGS sample) to the reference conditional
+#	population database of strains using bowtie2.
+#	Samtools is used in the end to sort and index the alignments, as well as to obtain basic stats ('flagstat').
+#
 #
 #   NOTES:
-#   Please see the dependencies section below for the required libraries (if any).
+#   The script uses the bowtie2 DNA aligner for the alignment step.  A bowtie2 index must have been previously created
+#	with the necessary conditional population stratins. Also, see the dependencies section below for the required
+#	binaries.
+#	Note that at the time this scrip was developed, Bowtie2 did not support large indices (it does so now).  So the app
+#	that created the index, "bowtie2-build", was limitted to creating indices from sequence databases smaller than
+#	4 GB, a 32-bit limitation.  This is the main reason why this script calls bowtie multiple times: so that it can
+#	align the reads to mutliple slices of the full index. The SAM/BAM results are then merged using samtools.
+#	Please see the paper supplementals for a detailed explanation.
+#
 #
 #   DEPENDENCIES:
 #
-#       • None. Standard bash shell script.
-#
+#       • sequencePermuter.pl — a Perl script that performs the point mutattions at the requested rate.
+#		• bowtie2 — a tool for aligning sequencing reads to reference sequences.
+#		• samtools — a suite of programs for interacting with high-throughput sequencing data
 #
 #
 #	AUTHOR:	Camilo Valdes (cvaldes3@med.miami.edu)
@@ -42,8 +59,7 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 
-# Run Variables
-# date '+%m/%d/%y %H:%M:%S'
+# ------------------------------------------------------- Setup -------------------------------------------------------
 
 PROJECT_DIR='/Path/To/Project/software'
 
@@ -53,8 +69,12 @@ PROB=$2
 FASTA_READS_1='/Path/To/data/hmp/samples/mid_vaginal/SRS015072/SRS015072-1.fa'
 FASTA_READS_2='/Path/To/data/hmp/samples/mid_vaginal/SRS015072/SRS015072-2.fa'
 
-
 PROC_THREADS=64
+
+echo ""
+echo "[" `date '+%m/%d/%y %H:%M:%S'` "]"
+echo "[" `date '+%m/%d/%y %H:%M:%S'` "] Starting..."
+echo "[" `date '+%m/%d/%y %H:%M:%S'` "]"
 
 #
 # Permute the Reads (paired-ends) IFF its not the baseline (0) run
@@ -67,10 +87,8 @@ if [ $PROB != 0 ]; then
 	mv $OUTPUT_DIR_READS/permutedSequences.fasta $OUTPUT_DIR_READS/permutedSequences-2.fasta
 fi
 
-#==============================================================================================================
-#
-#	Alignment Run
-#
+
+# --------------------------------------------------- DNA Alignment ---------------------------------------------------
 
 #	SAM Header Location
 HEADER='/Path/To/annotations/img/4/bacteria/metadata/header.sam'
@@ -121,9 +139,7 @@ FILENAME_2_B_2="alignments_2_B_2"
 INDEX_2_B_2=/projects/bioinf/ref/annotations/img/4/bacteria/index/2/B/2/img_bacteria_2_B_2
 
 
-#------------------------------------------------------------------------------------------------------------
-#
-# 	Bowtie Runs
+# ------------------------------------------------------- Bowtie ------------------------------------------------------
 
 # 	Parameters for bowtie
 SEED_LENGTH=20
@@ -138,9 +154,7 @@ SEED_INTERVAL="S,1,0.50"
 /nethome/bioinfo/tools/bowtie/v2/2.1.0/bowtie2 --local -D 20 -R 3 -N 0 -L $SEED_LENGTH -i $SEED_INTERVAL -a --time -f -x $INDEX_2_B_2 -1 $READS_1 -2 $READS_2 -S $OUTDIR/$FILENAME_2_B_2.sam
 
 
-#------------------------------------------------------------------------------------------------------------
-#
-# 	SAM to BAM & Rehead
+# ------------------------------------------------------- Samtools -----------------------------------------------------
 
 # 	Convert the results for INDEX_1_A, INDEX_1_B_1, INDEX_1_B_2
 /nethome/bioinfo/tools/samtools/0.1.19/samtools view -b -S -o $OUTDIR/$FILENAME_1_A.bam $OUTDIR/$FILENAME_1_A.sam
@@ -163,10 +177,10 @@ SEED_INTERVAL="S,1,0.50"
 /nethome/bioinfo/tools/samtools/0.1.19/samtools reheader $HEADER $OUTDIR/$FILENAME_2_B_1.bam > $OUTDIR/$FILENAME_2_B_1.reheaded.bam
 /nethome/bioinfo/tools/samtools/0.1.19/samtools reheader $HEADER $OUTDIR/$FILENAME_2_B_2.bam > $OUTDIR/$FILENAME_2_B_2.reheaded.bam
 
-#------------------------------------------------------------------------------------------------------------
-#
-# 	Merge the BAM files
 
+# ------------------------------------------------- SAM/BAM Processing ------------------------------------------------
+
+# 	Merge the BAM files
 /nethome/bioinfo/tools/samtools/0.1.19/samtools merge -f $OUTDIR/alignments-1.bam $OUTDIR/$FILENAME_1_A.reheaded.bam $OUTDIR/$FILENAME_1_B_1.reheaded.bam $OUTDIR/$FILENAME_1_B_2.reheaded.bam
 /nethome/bioinfo/tools/samtools/0.1.19/samtools merge -f $OUTDIR/alignments-2.bam $OUTDIR/$FILENAME_2_A.reheaded.bam $OUTDIR/$FILENAME_2_B_1.reheaded.bam $OUTDIR/$FILENAME_2_B_2.reheaded.bam
 /nethome/bioinfo/tools/samtools/0.1.19/samtools merge -f $OUTDIR/alignments.bam $OUTDIR/alignments-1.bam $OUTDIR/alignments-2.bam
@@ -211,9 +225,7 @@ SEED_INTERVAL="S,1,0.50"
 /nethome/bioinfo/tools/samtools/0.1.19/samtools idxstats $OUTDIR/$FILENAME_2_B_2.sorted.bam > $OUTDIR/reference_Counts-$FILENAME_2_B_2.txt
 
 
-#------------------------------------------------------------------------------------------------------------
-#
-# 	Cleanup — Remove files that we no longer need
+# ------------------------------------------------------ Clean Up -----------------------------------------------------
 
 rm -fR $OUTDIR/$FILENAME_1_A.sam
 rm -fR $OUTDIR/$FILENAME_1_A.bam
@@ -242,7 +254,10 @@ rm -fR $OUTDIR/$FILENAME_2_B_2.reheaded.bam
 rm -fR $OUTDIR/alignments.bam
 rm -fR $OUTDIR/alignments-MAPQ30.bam
 
-#------------------------------------------------------------------------------------------------------------
-#
-#	End of line
-#
+# -------------------------------------------------------- End --------------------------------------------------------
+
+echo "[" `date '+%m/%d/%y %H:%M:%S'` "]"
+echo "[" `date '+%m/%d/%y %H:%M:%S'` "]"
+echo "[" `date '+%m/%d/%y %H:%M:%S'` "] Done."
+echo "[" `date '+%m/%d/%y %H:%M:%S'` "]"
+echo ""
